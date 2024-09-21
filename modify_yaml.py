fbe34fe02f5daa4859e1412f3a9faf0a06790000
Emirhan Oğul <emirhan.ogul@metu.edu.tr>
2024-09-21 23:18:16 +0300

    update logger component

--- modify_yaml.py
+++ modify_yaml.py
@@ -5,13 +5,13 @@ import logger.utils
 
 
 def main():
-    input_yaml_filename = sys.argv[1]
-    output_yaml_filename = sys.argv[2]
-    repo = sys.argv[3]
-
     if len(sys.argv) != 4:
         print("Usage: python modify_yaml.py <yaml_filename> <output_filename> <repo>")
         sys.exit(1)
+
+    input_yaml_filename = sys.argv[1]
+    output_yaml_filename = sys.argv[2]
+    repo = sys.argv[3]
 
     if not os.path.isfile(input_yaml_filename):
         print("Please provide a valid YAML file.")
