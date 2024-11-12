2c46d6801c13fe1f0ab43b2152a3d2ff237c4f8a
Talank <tbaral@gmu.edu>
2024-11-12 17:26:23 -0500

    Updated run.sh to use actual workflow file

--- run.sh
+++ run.sh
@@ -25,21 +25,23 @@ path_to_yaml_file=$(echo "$output_yaml_filename" | rev | cut -d'/' -f1-3 | rev)
 path_to_local_repo=$(echo "$output_yaml_filename" | rev | cut -d'/' -f4- | rev)
 workflow_file=$(echo "$input_yaml_filename" | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
 
-run_id=$(gh run list --repo "$owner"/"$repo" --workflow "$path_to_yaml_file" --limit 1 --json databaseId --jq '.[0].databaseId')
-
 branch=$(git -C "$path_to_local_repo" rev-parse --abbrev-ref HEAD)
 git -C "$path_to_local_repo" fetch
 git -C "$path_to_local_repo" rebase origin/"$branch"
-git -C "$path_to_local_repo" push origin "$branch":"$branch"
+git -C "$path_to_local_repo" push origin "$branch"
 git -C "$path_to_local_repo" add "$path_to_yaml_file"
 git -C "$path_to_local_repo" commit -m "add modified YAML file"
-git -C "$path_to_local_repo" push
+git -C "$path_to_local_repo" push --set-upstream origin "$branch"
 
 echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Pushed the modified YAML file to remote repository."
+echo "Running the command gh run list --repo $owner/$repo --workflow $path_to_yaml_file --limit 1 --json databaseId --jq '.[0].databaseId'"
+$filename=$(echo "$path_to_yaml_file" | rev | cut -d'/' -f1 | rev)
+run_id=$(gh run list --repo "$owner"/"$repo" --workflow "$filename" --limit 1 --json databaseId --jq '.[0].databaseId')
+echo "[INFO] got the run id for the original YAML workflow: $run_id"
 
 while true; do
   echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Waiting until modified YAML workflow starts."
-  temp_run_id=$(gh run list --repo "$owner"/"$repo" --workflow "$path_to_yaml_file" --limit 1 --json databaseId --jq '.[0].databaseId')
+  temp_run_id=$(gh run list --repo "$owner"/"$repo" --workflow "$filename" --limit 1 --json databaseId --jq '.[0].databaseId')
   if [ "$run_id" != "$temp_run_id" ]; then
     echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Modified YAML workflow started."
     run_id=$temp_run_id
@@ -49,6 +51,8 @@ while true; do
 done
 
 echo "$run_id" > "$repo"-"$workflow_file".txt
+
+# path_to_yaml_file=$(echo "$output_yaml_filename" | rev | cut -d'/' -f1-3 | rev)
 
 # wait until modified yaml workflow finishes
 while true; do
@@ -74,24 +78,34 @@ rm -rf "$output_file"
 mkdir -p "$repo"-"$workflow_file"
 mkdir -p "$repo"
 # download inotifywait log to "$repo"/inotifywait-${{ job_name }}/inotifywait-log-${{ job_name }}.csv
+echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Downloaded the inotifywait log artifacts."
 gh run download "$run_id" --repo "$owner"/"$repo" -D "$repo"-"$workflow_file"
 
-echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Downloaded the inotifywait log artifacts."
 echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") | Finding unused directories and their responsible plugins."
 
 while IFS=' ' read -r job_id name; do
   # fetch workflow run logs to "$repo"/workflow-run-log.txt
+  echo "downloading logs: using command"
+  echo "curl -s -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer $GITHUB_API_TOKEN\" -H \"X-GitHub-Api-Version: 2022-11-28\" -L \"https://api.github.com/repos/$owner/$repo/actions/jobs/$job_id/logs\" -o \"$repo\"/workflow-run-log-\"$name\"-\"$workflow_file\".txt"
+
+  echo $job_id
   curl -s -H "Accept: application/vnd.github+json" \
        -H "Authorization: Bearer $GITHUB_API_TOKEN" \
        -H "X-GitHub-Api-Version: 2022-11-28" \
        -L "https://api.github.com/repos/$owner/$repo/actions/jobs/$job_id/logs" \
        -o "$repo"/workflow-run-log-"$name"-"$workflow_file".txt
 
+  echo "[INFO] downloading the inotifywait log for $name into $repo/inotifywait-$name/inotifywait-log-$name.csv"
+
+
   if [ "$output_file" != "" ]; then
     echo "Unused directories and their responsible plugins in $name:" >> "$output_file"
   else
     echo "Unused directories and their responsible plugins in $name:"
   fi
+  echo "path to inotifywait log is: $repo"-"$workflow_file"/inotifywait-"$name"/inotifywait-log-"$name".csv"
+  echo "path to workflow run log is: $repo"/workflow-run-log-"$name"-"$workflow_file".txt"
+
   python find_plugins.py "$repo"-"$workflow_file"/inotifywait-"$name"/inotifywait-log-"$name".csv "$repo"/workflow-run-log-"$name"-"$workflow_file".txt "$output_file" "$input_yaml_filename" "$name"
   ret_val=$?
   if [ $ret_val -eq 1 ]; then
